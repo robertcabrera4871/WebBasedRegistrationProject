c33a33358ed5446d18f8b3666d6b99ab15f6a277
Robert Cabrera <42824688+robertcabrera4871@users.noreply.github.com>
2021-09-07 20:15:03 -0400

    start of project

---
+++ my-app/src/components/Homepage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Homepage.css';
+
+function Homepage(){
+    return (
+     <div>
+     <header>SUNY Hogwarts</header>
+     <h1>Academic Calendar</h1>
+     </div>
+    );
+
+}
+
+export default Homepage
