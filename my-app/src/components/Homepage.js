0b51efa68622f336e9b5b770ad3d6765637c26a5
Robert Cabrera <42824688+robertcabrera4871@users.noreply.github.com>
2021-09-09 21:18:57 -0400

    worked on homepage

--- my-app/src/components/Homepage.js
+++ my-app/src/components/Homepage.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import './Homepage.css';
+import schedule from '../assets/fakeSchedule.png';
+import groupLog from '../assets/groupLog.png'
 
 function Homepage(){
     return (
@@ -9,20 +11,24 @@ function Homepage(){
           <li><a href="news.asp">News</a></li>
           <li><a href="contact.asp">Contact</a></li>
           <li><a href="about.asp">About</a></li>
+          <li><a href="about.asp">Login</a></li>
           <li><img src="https://cdn11.bigcommerce.com/s-ydriczk/images/stencil/original/products/88365/91138/Harry-Potter-Hogwarts-Crest-Official-wall-mounted-cardboard-cutout-buy-now-at-star__49592.1507641303.jpg?c=2" class="logo"/></li>
         </ul>
-             <header>SUNY Hogwarts</header>
-           <img src="http://wagner.edu/about/wp-content/blogs.dir/54/files/2005/11/10675scr.jpg"/>
-             <h1>Academic Calendar</h1>
-             <img src="https://i.etsystatic.com/10950629/r/il/1081ac/2535486844/il_794xN.2535486844_dzuf.jpg"/>
-           <h1>Schedule Search</h1>
+             <header class="bar">SUNY Hogwarts</header>
+           <img class="schoolImg" src="http://wagner.edu/about/wp-content/blogs.dir/54/files/2005/11/10675scr.jpg"/>
+             <h1 class="bar">Academic Calendar</h1>
+             <img src="https://www.academycollege.edu/wp-content/uploads/2020/10/2021-Academic-Calendar.png"/>
+           <h1 class="bar">Schedule Search</h1>
            <label>Chose a semester</label>
            <select>
            <option>Fall 2021</option>
            <option>Spring 2022</option>
            </select> <br/><br/>
-           <button>Search</button>
-             </div>
+           <button>Search</button><br/><br/>
+           <img class="schedule" src={schedule}/>
+           <h1 class="bar">Group Log</h1>
+           <img class="schedule" src={groupLog}/>
+            </div>
     );
 
 }
